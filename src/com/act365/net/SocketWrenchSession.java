f2d34ac0975b221e61f7fafeb18a26a9f3c4c957
Martin Highmore <Martin.Highmore@act365.com>
2004-09-02 21:14:41 +0000

    Static protocol functions moved here from SocketUtils.

--- src/com/act365/net/SocketWrenchSession.java
+++ src/com/act365/net/SocketWrenchSession.java
@@ -26,6 +26,9 @@
 
 package com.act365.net;
 
+import com.act365.net.tcp.RawTCPSocketImplFactory ;
+
+import java.io.IOException ;
 import java.net.*;
 
 /**
@@ -66,5 +69,174 @@ public class SocketWrenchSession {
   }
 
   native static int _shutdown() throws SocketException ;
+  /**
+   * The protocol in use in the current session.
+   */
+    
+  static int protocol = 0 ;
+    
+  static boolean includeheader = false ,
+                 isRaw = false ;
+    
+  /**
+   * Sets the factory classes for <code>Socket</code>, <code>ServerSocket</code>
+   * and <code>DatagramSocket</code> according to the choice of protocol. 
+   * The TCP, TCP/J, UDP and ICMP protocols are supported. (TCP/J is a clone
+   * of TCP that uses the IP protocol code 156). The addition of the 'Raw' 
+   * prefix to the protocol name indicates that a raw socket is used in the
+   * underlying implementation, in which case the user will have to construct
+   * the protocol-specific header for transmitted packets. The addition of
+   * the 'Hdr' prefix to the protocol indicates that the user wishes to
+   * construct the IP header in addition to the protocol-specific header.
+   *   
+   * The <code>Socket</code> and <code>ServerSocket</code> factory objects 
+   * will be set for TCP-based protocols.
+   * 
+   * @param proto protocol to be used in the app
+   */
+    
+  public static void setProtocol( int proto ) throws IOException {
+        
+      switch( proto ){
+            
+          case SocketConstants.JSWPROTO_NULL:
+              break;
+            
+          case SocketConstants.JSWPROTO_ICMP:
+              protocol = SocketConstants.IPPROTO_ICMP ;
+              includeheader = false ;
+              isRaw = true ; 
+              DatagramSocket.setDatagramSocketImplFactory( new ICMPDatagramSocketImplFactory() );
+              break;
+                
+          case SocketConstants.JSWPROTO_HDRICMP:
+              protocol = SocketConstants.IPPROTO_ICMP ;
+              includeheader = true ;
+              isRaw = true ;
+              DatagramSocket.setDatagramSocketImplFactory( new HdrICMPDatagramSocketImplFactory() );
+              break;
+                
+          case SocketConstants.JSWPROTO_JDKTCP:
+              protocol = SocketConstants.IPPROTO_TCP ;
+              includeheader = false ;
+              isRaw = false ;
+              break;
+                
+          case SocketConstants.JSWPROTO_TCP:
+              protocol = SocketConstants.IPPROTO_TCP ;
+              includeheader = false ;
+              isRaw = false ;
+              Socket.setSocketImplFactory( new TCPSocketImplFactory() );
+              ServerSocket.setSocketFactory( new TCPSocketImplFactory() );
+              break;
+                
+          case SocketConstants.JSWPROTO_RAWTCP:
+              protocol = SocketConstants.IPPROTO_TCP ;
+              includeheader = false ;
+              isRaw = true ;
+              DatagramSocket.setDatagramSocketImplFactory( new RawTCPDatagramSocketImplFactory() );
+              Socket.setSocketImplFactory( new RawTCPSocketImplFactory() );
+              ServerSocket.setSocketFactory( new RawTCPSocketImplFactory() );
+              break;
+                
+          case SocketConstants.JSWPROTO_RAWHDRTCP:
+              protocol = SocketConstants.IPPROTO_TCP ;
+              includeheader = true ;
+              isRaw = true ;
+              DatagramSocket.setDatagramSocketImplFactory( new RawHdrTCPDatagramSocketImplFactory() );
+              Socket.setSocketImplFactory( new RawTCPSocketImplFactory() );
+              ServerSocket.setSocketFactory( new RawTCPSocketImplFactory() );
+              break;
+                
+          case SocketConstants.JSWPROTO_RAWTCPJ:
+              protocol = SocketConstants.IPPROTO_TCPJ ;
+              includeheader = false ;
+              isRaw = true ;
+              DatagramSocket.setDatagramSocketImplFactory( new RawTCPJDatagramSocketImplFactory() );
+              Socket.setSocketImplFactory( new RawTCPSocketImplFactory() );
+              ServerSocket.setSocketFactory( new RawTCPSocketImplFactory() );
+              break;
+                
+          case SocketConstants.JSWPROTO_RAWHDRTCPJ:
+              protocol = SocketConstants.IPPROTO_TCPJ ;
+              includeheader = true ;
+              isRaw = true ;
+              DatagramSocket.setDatagramSocketImplFactory( new RawHdrTCPJDatagramSocketImplFactory() );
+              Socket.setSocketImplFactory( new RawTCPSocketImplFactory() );
+              ServerSocket.setSocketFactory( new RawTCPSocketImplFactory() );
+              break;
+                
+          case SocketConstants.JSWPROTO_JDKUDP:
+              protocol = SocketConstants.IPPROTO_UDP ;
+              includeheader = false ;
+              isRaw = false ;
+              break;
+                
+          case SocketConstants.JSWPROTO_UDP:
+              protocol = SocketConstants.IPPROTO_UDP ;
+              includeheader = false ;
+              isRaw = false ;
+              DatagramSocket.setDatagramSocketImplFactory( new UDPDatagramSocketImplFactory() );
+              break;
+                
+          case SocketConstants.JSWPROTO_RAWUDP:
+              protocol = SocketConstants.IPPROTO_UDP ;
+              includeheader = false ;
+              isRaw = true ;
+              DatagramSocket.setDatagramSocketImplFactory( new RawUDPDatagramSocketImplFactory() );
+              break;
+                
+          case SocketConstants.JSWPROTO_RAWHDRUDP:
+              protocol = SocketConstants.IPPROTO_UDP ;
+              includeheader = true ;
+              isRaw = true ;
+              DatagramSocket.setDatagramSocketImplFactory( new RawHdrUDPDatagramSocketImplFactory() );
+              break;
+                
+          default:
+              throw new IOException("Protocol is not supported");
+      }
+  }
+    
+  /**
+   * Sets the app protocol by label
+   * @see setProtocol(int)
+   */
+
+  public static void setProtocol( String protocol ) throws IOException {
+      int i = 0 , proto = -1 ;
+      while( i < SocketConstants.jswProtocolLabels.length ){
+          if( protocol.equalsIgnoreCase( SocketConstants.jswProtocolLabels[ i ] ) ){
+              setProtocol( i );
+              return;
+          }
+          ++ i ;
+      }
+      throw new IOException("Protocol " + protocol + " is not supported");
+  }
+    
+  /**
+   Returns the protocol associated with the current <code>DatagramSocketImpl</code>.
+  */
+
+  public static int getProtocol() {
+    return protocol ;
+  }
+    
+  /**
+   Indicates whether the chosen protocol requires the user to include the IP header.
+  */
+
+  public static boolean includeHeader() {
+    return includeheader ;
+  } 
+
+  /**
+   * Indicates whether the current protocol creates raw sockets.
+   */
+    
+  public static boolean isRaw() {
+      return isRaw ;
+  }
 }
 
